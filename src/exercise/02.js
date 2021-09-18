7a74836d29b72dec25589821f991ac6b27d5c083
Yetrina Battad <hello@yetti.io>
2021-09-18 11:16:29 +1000

    feat(ex02): complete exercise 02
    skipped Extra Credit 4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,15 +3,18 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, initialValue) {
+  const [name, setName] = React.useState(() => window.localStorage.getItem(key) || initialValue)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, name)
+  }, [name])
+
+  return [name, setName]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
-
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
